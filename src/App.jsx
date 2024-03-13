cbfbc3663c4be9fb1457224be0622de2e3e67ae7
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-03-13 17:34:12 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from "react";
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+
+  return (
+    <>
+    <React.Fragment>
+    <NavBar/>
+    </React.Fragment>
+    
+      <h1>COMPONENTS</h1>
+    </>
+  )
+}
+
+export default App
