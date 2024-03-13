9b554fb4e392aa19beed732743f5530f9c0b0dc4
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-03-13 23:33:01 +0100

    Modified:NavBar.jsx navbar.module.css):corrección  de error en visibilidad del menu responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,28 +3,28 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 import styles from './navbar.module.css';
 
 const NavBar = () => {
-    const navref = useRef();
+    const navRef = useRef();
 
     const showNavBar = () => {
-        navref.current.classList.toggle(styles.responsive_nav);
+        navRef.current.classList.toggle(styles.responsive_nav);
     };
 
     return (
         <header>
             <h3>Logo</h3>
-            <nav ref={navref} className={styles.nav}>
-                <a href="#">Home</a>
-                <a href="#">Aboutme</a>
-                <a href="#">My work</a>
-                <a href="#">Contact</a>
+            <nav ref={navRef} >
+                <a href="/#">Home</a>
+                <a href="/#">Aboutme</a>
+                <a href="/#">My work</a>
+                <a href="/#">Contact</a>
 
                 <button className={`${styles.nav_btn} ${styles.nav_close_btn}`} onClick={showNavBar}>
-                    <FaTimes />
-                </button>
-                <button className={`${styles.nav_btn} ${styles.nav_open_btn}`} onClick={showNavBar}>
-                    <FaBars />
+                    <FaTimes className={styles.icon}/>
                 </button>
             </nav>
+            <button className={styles.nav_btn} onClick={showNavBar}>
+                    <FaBars className={styles.icon}/>
+                </button>
         </header>
     );
 };
