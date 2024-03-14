ace86cc446dab6a6f0d19e423786e588c71041f1
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-03-14 18:23:26 +0100

    modified(NavBar.jsx navbar.module.css):estados a menu responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,32 +1,79 @@
-import { useRef } from 'react';
+/* import { useRef } from 'react'; 
 import { FaBars, FaTimes } from 'react-icons/fa';
 import styles from './navbar.module.css';
 
 const NavBar = () => {
     const navRef = useRef();
 
-    const showNavBar = () => {
+const showNavBar = () => {
         navRef.current.classList.toggle(styles.responsive_nav);
-    };
+    }; 
 
     return (
         <header>
-            <h3>Logo</h3>
-            <nav ref={navRef} >
+            <h3>NavBar <span>Style module</span></h3>
+            <div>
+            <nav ref={navRef} className={styles.nav}>
                 <a href="/#">Home</a>
                 <a href="/#">Aboutme</a>
-                <a href="/#">My work</a>
                 <a href="/#">Contact</a>
+                <button className={styles.logInBtn}>Log in</button>
+                <button className={styles.signInBtn}>Sign in</button>
 
-                <button className={`${styles.nav_btn} ${styles.nav_close_btn}`} onClick={showNavBar}>
+                <button className={`${styles.navBtn} ${styles.navCloseBtn}`} onClick={showNavBar}>
                     <FaTimes className={styles.icon}/>
                 </button>
             </nav>
-            <button className={styles.nav_btn} onClick={showNavBar}>
+
+            <button className={styles.navBtn} onClick={showNavBar}>
                     <FaBars className={styles.icon}/>
                 </button>
+            </div>
+            
         </header>
     );
 };
 
 export default NavBar;
+
+
+ */
+
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import styles from './navbar.module.css';
+
+const NavBar = () => {
+const [showMenu, setShowMenu] = useState(false);
+
+const handleClick = () => {
+    setShowMenu(!showMenu)
+}
+    return (
+        <header>
+            <h3>NavBar <span>Style module</span></h3>
+            <div>
+                <nav className={`${styles.nav} ${showMenu ? styles.navResponsive : ''}`}>
+                    <a href="/#">Home</a>
+                    <a href="/#">Aboutme</a>
+                    <a href="/#">Contact</a>
+                    <button className={styles.logInBtn}>Log in</button>
+                    <button className={styles.signInBtn}>Sign in</button>
+
+                     <button className={styles.faTimes} onClick={handleClick}>
+                    <FaTimes/>
+                </button> 
+                </nav>
+
+                <button className={styles.faBars} onClick={handleClick}>
+                    <FaBars />
+                </button>
+            </div>
+
+        </header>
+    );
+};
+
+export default NavBar;
+
+
